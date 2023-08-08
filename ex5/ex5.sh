998dfa6b8b8ed183bb66cc4a039c01aef4afc7d2
vboxuser <vboxuser@Ubuntu-Sara.myguest.virtualbox.org>
2023-08-08 14:08:42 +0300

    finish-4-end

--- ex5/ex5.sh
+++ ex5/ex5.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #ex5
+<<<<<<< HEAD
 rows=$((1 + RANDOM%10))
 cols=$((1 + RANDOM%10))
 
@@ -22,24 +23,3 @@ for((i = 0;i < rows;i++)); do
   echo
 done
 
-rows=$((1 + RANDOM%10))
-cols=$((1 + RANDOM%10))
-
-delclare -A matrix
-
-generate_random_number(){
-  echo $((1 + RANDOM%100))
-}
-
-for((i = 0;i < rows;i++); do
-  for((j = 0;j < cols;j++)); do
-    matrix[$i,$j]=$(generate_random_number)
-  done
-done
-
-for((i = 0;i < rows;i++)); do
-  for((j = 0;j < cols;j++)); do
-    printf"%-5s" "${matrix[$i,$j]}"
-  done
-  echo
-done
