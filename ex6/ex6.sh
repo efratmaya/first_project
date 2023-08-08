cf557ec90bd93a1bc3214d9157d3637c7b4d54b9
Shira <shira@Ubunto23.10.myguest.virtualbox.org>
2023-08-08 13:58:34 +0300

    ex6 finished

--- ex6/ex6.sh
+++ ex6/ex6.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 #ex6
-echo "start" 
+
+users=$(cat /etc/passwd)
+line=0
+c=0
+declare -A user_mat
+
+for user in $users; do
+   old_ifs= $IFS
+   IFS=":"
+   column=0
+   for field in $user; do
+      user_mat[$line,$column]=$field
+      ((column++))
+      c=$column
+    done
+    ((line++)) 
+    IFS=$old_ifs
+done 
+
+for ((i=1; i<=line; i++)) do
+    for ((j=1; j<=c; j++)) do
+        printf "%s " "${user_mat[$i,$j]}       "
+    done
+    echo
+done           
+
 
 
